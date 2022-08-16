4efae4071ed4cd2f1593d9f7414b5e94563acabd
Ramon M Cott <ramoncott@hotmail.com>
2022-08-16 18:40:26 -0400

    Adding EJS template engine

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home-guest')
+})
+
+app.listen(3000)
