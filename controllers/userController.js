2565a7ece60119097c3df5b6fcbdabe032eedc18
Ramon M Cott <ramoncott@hotmail.com>
2022-08-20 17:09:15 -0400

    Promises implementation Part 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,27 @@
 const path = require('path')
-const User = require(path.join(appRoot, 'models/User'))
+const User = require(path.join(appRoot, 'tables/User'))
 
 exports.home = (req, res) => {
     res.render('home-guest')
 }
 
-exports.register = (req, res) =>{
+exports.register = (req, res) => {
     let user = new User(req.body)
-    user.register()
-    if (user.errors.length) {
-        res.send(user.errors)        
-    } else {
-        res.send("No errors.")
-    }
+    user.register().then((result) => {
+        res.send(result)
+    }).catch((error) => {
+        user.errors.push(error)
+        res.send(user.errors)
+    })
 }
+
+exports.login = (req, res) => {
+    let user = new User(req.body)
+    user.login().then((result) => {
+        res.send(result)
+    }).catch((error) => {
+        user.errors.push(error)
+        res.send(user.errors)
+    })
+}
+
--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const {home, register} = require(path.join(appRoot, 'controllers/userController'))
+const {home, register, login} = require(path.join(appRoot, 'controllers/userController'))
 
 exports.home = router.get('/', home)
 exports.register = router.post('/register', register)
+exports.login =  router.post('/login', login)
+
 
