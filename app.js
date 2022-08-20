e8144b38553b863312714ad85a248eaedf981b8c
Ramon M Cott <ramoncott@hotmail.com>
2022-08-20 18:01:26 -0400

    Add use login

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path')
 require(path.join(__dirname, 'globals'))
 
 // External files
-const {home, register} = require(path.join(appRoot, 'router'))
+const {home, register, login} = require(path.join(appRoot, 'router'))
 app.use(express.static(path.join(appRoot, 'public')))
 app.set('views', path.join(appRoot, 'views'))
 
@@ -21,6 +21,7 @@ app.use(express.json())
 // Routes
 app.use('/', home)
 app.use('/register', register)
+app.use('/login', login)
 
 // Listen port
 let port = process.env.PORT
