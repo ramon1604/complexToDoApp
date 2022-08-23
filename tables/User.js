9283d9ab82e8e1b8cb2b2cab66452c906a844ea8
Ramon M Cott <ramoncott@hotmail.com>
2022-08-23 14:53:24 -0400

    Export User class

--- tables/User.js
+++ tables/User.js
@@ -2,15 +2,15 @@
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 
-module.exports = class User {
+class User {
     constructor(data) {
         this.data = data
         this.errors = []
     }
     cleanUp() {
-        if (typeof (this.data.username) != "string") { this.data.username = ""} 
-        if (typeof (this.data.email) != "string") { this.data.email = ""} 
-        if (typeof (this.data.password) != "string") { this.data.password = ""} 
+        if (typeof (this.data.username) != "string") { this.data.username = "" }
+        if (typeof (this.data.email) != "string") { this.data.email = "" }
+        if (typeof (this.data.password) != "string") { this.data.password = "" }
 
         // get rid of any bogus properties
         this.data = {
@@ -20,19 +20,19 @@ module.exports = class User {
         }
     }
     async validate() {
-        if (this.data.username == "") { this.errors.push('Username required.')} 
-        if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push('Username may only contain letters and numbers.')} 
-        if (!validator.isEmail(this.data.email)) { this.errors.push('Valid email required.')} 
-        if (this.data.password == "") { this.errors.push('Password required.')} 
-        if (this.data.password.length > 0 && this.data.password.length <= 8) { this.errors.push('Password must be greater than 8 characters.')} 
-        if (this.data.password.length > 30) { this.errors.push('Password can not exceed 30 characters and contain letters and numbers.')} 
-        if (this.data.username.length > 0 && this.data.username.length <= 4) { this.errors.push('Username must be greater than 4 characters.')} 
-        if (this.data.username.length > 20) { this.errors.push('Username can not exceed 20 characters.')} 
+        if (this.data.username == "") { this.errors.push('Username required.') }
+        if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push('Username may only contain letters and numbers.') }
+        if (!validator.isEmail(this.data.email)) { this.errors.push('Valid email required.') }
+        if (this.data.password == "") { this.errors.push('Password required.') }
+        if (this.data.password.length > 0 && this.data.password.length <= 8) { this.errors.push('Password must be greater than 8 characters.') }
+        if (this.data.password.length > 30) { this.errors.push('Password can not exceed 30 characters and contain letters and numbers.') }
+        if (this.data.username.length > 0 && this.data.username.length <= 4) { this.errors.push('Username must be greater than 4 characters.') }
+        if (this.data.username.length > 20) { this.errors.push('Username can not exceed 20 characters.') }
         if (!this.errors.length) {
             const userExists = await db.collection("users").findOne({ username: this.data.username })
-            if (userExists) { this.errors.push('Username already taken.')} 
+            if (userExists) { this.errors.push('Username already taken.') }
             const emailExists = await db.collection("users").findOne({ email: this.data.email })
-            if (emailExists) { this.errors.push('Email already taken.')} 
+            if (emailExists) { this.errors.push('Email already taken.') }
         }
 
     }
@@ -68,4 +68,4 @@ module.exports = class User {
     }
 }
 
-
+module.exports = User
