731eac4c51c4457aa47406a122cff708026bd713
Ramon M Cott <ramoncott@hotmail.com>
2022-09-08 15:59:14 -0400

    Optimizing browser code

--- browser/js/functions.js
+++ browser/js/functions.js
@@ -2,3 +2,18 @@
 function enaBtn(id) {
     document.getElementById(id).disabled = false;
 }
+
+async function alertMsg(title, text, icon, buttons, mode, formId) {
+    await swal(
+        {
+            title: title,
+            text: text,
+            icon: icon,
+            buttons: buttons,
+            dangerMode: mode
+        }).then(
+            (value) => {
+                if (value) { document.getElementById(formId).submit() };
+            }
+        )
+}
