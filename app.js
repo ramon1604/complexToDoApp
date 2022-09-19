0e4ccc17471741576a3f8f56b0ff5d345618593d
Ramon M Cott <ramoncott@hotmail.com>
2022-09-19 16:51:42 -0400

    Finish implementing socket.io chat

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const sanitizeHTML = require('sanitize-html')
 
 //Load globals
 require(path.join(__dirname, 'globals'))
@@ -33,9 +34,27 @@ app.use(express.json())
 const { homeRoute } = require(path.join(appRoot, 'router'))
 app.use('/', homeRoute)
 
+// Listen for socket connection
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+io.use((socket, next) => {
+  sessionOptions(socket.request, socket.request.res, next)
+})
+io.on('connection', (socket) => {
+  if (socket.request.session.user) {
+     let loggedUser = []
+     loggedUser = socket.request.session.user
+     socket.emit('loggedUser', loggedUser)
+     socket.on('chatMsgFromBrowser', (data) => {
+      data.user = socket.request.session.user
+      socket.broadcast.emit('chatMsgFromServer', { message: sanitizeHTML(data.message), user: data.user })
+    })
+  }
+})
+
 // Listen port
 let port = process.env.PORT
 if (port == null || port == "") {
   port = 3000
 }
-app.listen(port)
+server.listen(port)
--- browser/js/classes/chat.js
+++ browser/js/classes/chat.js
@@ -3,14 +3,31 @@ class Chat {
         this.openedYet = false
         this.chatWrapper = document.querySelector("#chat-wrapper")
         this.injectHTML()
+        this.chatLog = document.querySelector("#chat")
+        this.chatField = document.querySelector("#chatField")
+        this.chatForm = document.querySelector("#chatForm")
         this.chatIcon = document.querySelector(".header-chat-icon")
         this.chatIconClose = document.querySelector(".chat-title-bar-close")
         this.events()
     }
 
     events() {
+        this.chatForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            this.sendMessageToServer()
+
+        })
         this.chatIcon.addEventListener('click', () => this.showHideChat())
         this.chatIconClose.addEventListener('click', () => this.hideChat())
+    }
+
+    sendMessageToServer() {
+        if (this.chatField.value) {
+            this.msgFromBrowser()
+            this.socket.emit('chatMsgFromBrowser', { message: this.chatField.value })
+            this.chatField.value = ''
+            this.chatField.focus()
+        }
     }
 
     injectHTML() {
@@ -24,8 +41,42 @@ class Chat {
     }
 
     openConnection() {
+        this.socket = io()
+        this.socket.on('loggedUser', loggedUser => {
+            this.loggedUser = loggedUser
+        })
+        this.socket.on('chatMsgFromServer', data => {
+            this.msgFromServer(data)
+        })
+    }
 
-    }    
+    msgFromBrowser() {
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+        <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
+          </div>
+        </div>
+        <strong class="ml-1">${this.loggedUser.username}</strong>
+        <img class="chat-avatar avatar-tiny" src="https://gravatar.com/avatar/${this.loggedUser.avatar}?s=128">
+      </div>
+        `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
+    }
+
+    msgFromServer(data) {
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+        <div class="chat-other">
+        <a href="/profile-posts/${data.user._id}/myPosts"><img class="avatar-tiny" src="https://gravatar.com/avatar/${data.user.avatar}?s=128"></a>
+        <div class="chat-message"><div class="chat-message-inner">
+          <a href="/profile-posts/${data.user._id}/myPosts"><strong>${data.user.username}:</strong></a>
+          ${data.message}
+        </div></div>
+      </div>
+        `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
+    }
 
     showHideChat() {
         if (!this.chatWrapper.classList.contains("chat--visible")) {
@@ -34,8 +85,9 @@ class Chat {
                 this.openedYet = true
             }
             this.chatWrapper.classList.add("chat--visible")
-        } else {  
-            this.chatWrapper.classList.remove("chat--visible")  
+            this.chatField.focus()
+        } else {
+            this.chatWrapper.classList.remove("chat--visible")
         }
     }
 
